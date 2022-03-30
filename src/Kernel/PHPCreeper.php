3e3d10a15fbe3c115ff815e3e3fe4fb5f49bee35
blogdaren <blogdaren@163.com>
2022-03-30 22:47:13 +0800

    fix OOM bug caused by the static array with phpQuery::$documents

--- src/Kernel/PHPCreeper.php
+++ src/Kernel/PHPCreeper.php
@@ -40,7 +40,7 @@ class PHPCreeper extends Worker
      *
      * @var string
      */
-    const  CURRENT_VERSION = '1.2.3';
+    const  CURRENT_VERSION = '1.3.0';
 
     /**
      * valid assemble package methods
